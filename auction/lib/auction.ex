550e867d9a64789aedc3e3cd9c80c7cf41aa4d55
jmtes <juliennetesoro@gmail.com>
2021-09-15 22:06:44 -0700

    Move all code into one file for now I couldn't compile while it was all separated and I'm not sure why

--- auction/lib/auction.ex
+++ auction/lib/auction.ex
@@ -28,3 +28,45 @@ defmodule Auction do
     @repo.get_by(Item, attrs)
   end
 end
+
+defmodule Auction.Item do
+  defstruct [:id, :title, :description, :ends_at]
+end
+
+# The repo is a mapping to a data store and the app's
+# gateway to the data inside the database, translating
+# what you'd like from the database into "database speak"
+# through a defined public interface
+
+# This is great because as long as an adapter for your
+# DB is available, the repo can talk many different
+# "database languages", but the public interface your
+# app uses to talk to the repo remains the same
+defmodule Auction.FakeRepo do
+  alias Auction.Item
+
+  @items [
+    %Item{
+      id: 1,
+      title: "Tomorrow Sex Will Be Good Again",
+      description: "A book by Katherine Angel",
+      ends_at: ~N[2022-01-01 12:00:00]
+    },
+    %Item{
+      id: 2,
+      title: "Weapons of Math Destruction",
+      description: "A book by Cathy O'Neil",
+      ends_at: ~N[2022-02-02 12:00:00]
+    },
+    %Item{
+      id: 3,
+      title: "Gone Girl",
+      description: "A book by Gillian Flynn",
+      ends_at: ~N[2022-03-03 12:00:00]
+    }
+  ]
+
+  # Note that pattern matching is at play in this function signature
+  # This function will only be called if the first argument is Item
+  def all(Item), do: @items
+end

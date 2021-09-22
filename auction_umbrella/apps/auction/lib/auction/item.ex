49f380e1f18e0d8dbcf56dcbca7a162c13c7a12e
jmtes <juliennetesoro@gmail.com>
2021-09-22 16:53:23 -0700

    Require title field

--- auction_umbrella/apps/auction/lib/auction/item.ex
+++ auction_umbrella/apps/auction/lib/auction/item.ex
@@ -12,5 +12,8 @@ defmodule Auction.Item do
     item
     # You can only change the item's title, description, and end time
     |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+    # This ensures that either the item struct or the changeset
+    # contains a title
+    |> Ecto.Changeset.validate_required(:title)
   end
 end

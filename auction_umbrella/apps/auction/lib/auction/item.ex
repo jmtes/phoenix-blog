eaa0474e7fc9ba0b8da83268a3e53c0f6c595ee3
jmtes <juliennetesoro@gmail.com>
2021-09-22 16:47:01 -0700

    Define changeset

--- auction_umbrella/apps/auction/lib/auction/item.ex
+++ auction_umbrella/apps/auction/lib/auction/item.ex
@@ -7,4 +7,10 @@ defmodule Auction.Item do
     field(:ends_at, :utc_datetime)
     timestamps()
   end
+
+  def changeset(item, params \\ %{}) do
+    item
+    # You can only change the item's title, description, and end time
+    |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+  end
 end

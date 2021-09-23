07042e0f974317f3d0d8d9280485757530883c29
jmtes <juliennetesoro@gmail.com>
2021-09-22 17:08:14 -0700

    Validate string fields

--- auction_umbrella/apps/auction/lib/auction/item.ex
+++ auction_umbrella/apps/auction/lib/auction/item.ex
@@ -1,6 +1,8 @@
 defmodule Auction.Item do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "items" do
     field(:title, :string)
     field(:description, :string)
@@ -11,9 +13,13 @@ defmodule Auction.Item do
   def changeset(item, params \\ %{}) do
     item
     # You can only change the item's title, description, and end time
-    |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+    |> cast(params, [:title, :description, :ends_at])
     # This ensures that either the item struct or the changeset
     # contains a title
-    |> Ecto.Changeset.validate_required(:title)
+    |> validate_required(:title)
+    # This ensures the title is at least 3 chars long
+    |> validate_length(:title, min: 3)
+    # This ensures the description is no longer than 200 chars
+    |> validate_length(:description, max: 200)
   end
 end
